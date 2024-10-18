4dc050917919d7e8d50089438bf5d7beddfb87bb
Rosario <arancio.rosario@gmail.com>
2024-10-18 08:10:45 +0200

    first commit

---
+++ LAB1/ex1/src/lab01_pkg/lab01_pkg/localization.py
@@ -0,0 +1,45 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Pose
+
+
+class Localization(Node):
+
+    def __init__(self):
+        super().__init__('localization')
+        self.publisher_ = self.create_publisher(Pose, '/pose', 10)
+        
+        msg = Pose()      
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    controller = Controller()
+
+    rclpy.spin(controller)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
